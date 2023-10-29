3a9bddec3e6d759c8a4595a09d63326fe198cd50
shunkominato <shun19871001@gmail.com>
2023-10-30 08:28:04 +0900

    a

---
+++ ent/schema/team.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Team holds the schema definition for the Team entity.
+type Team struct {
+	ent.Schema
+}
+
+// Fields of the Team.
+func (Team) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").Default("-"),
+	}
+}
+
+// Edges of the Team.
+func (Team) Edges() []ent.Edge {
+	return nil
+}
