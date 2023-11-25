7d9ba1d47d2b10112179c64f73dcda054bc3c06a
shunkominato <shun19871001@gmail.com>
2023-11-26 07:24:52 +0900

    a

--- internal/graph/resolver/resolver.go
+++ internal/graph/resolver/resolver.go
@@ -12,7 +12,7 @@ import (
 
 // This file will not be regenerated automatically.
 //
-// It serves as dependency injection for your app, add any dependencies you require here.
+// It serves as dependency injection for your app, add any dependencies you require here
 
 type Resolver struct{
 	todos []*model.Todo
